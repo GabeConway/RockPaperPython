42b07fcba50b67359e680c611239eea65ff8838c
RobiWanKenobi <94720060+RobiTheGit@users.noreply.github.com>
2023-08-02 13:44:25 -0600

    Updates
    Nicer text formatting
    Give rounds scored out of data at the end of the round
    When it asks your name, you can press enter, and it will get your username for the user you are logged in as
    Names are in title case

--- RockPaper.py
+++ RockPaper.py
@@ -1,9 +1,12 @@
+#!/usr/bin/python3
+
 # Rock Paper Python
 # Standard rock paper scissors game with two play modes, best out of three rounds. Random mode will choose a random selection.
 # Smart mode will base its choice on statistics from your last previous round.
 
-import random
 
+import random
+from getpass import getuser
 # Variable declaration
 scores={"User":0, "CPU":0}
 gameModes = ["random", "smart"]
@@ -20,47 +23,66 @@ options=[*beats]
 def scoreProcess(scores):
     print(f'Final Score User: {scores["User"]}, CPU: {scores["CPU"]}')
     if scores["User"] > scores["CPU"]:
-        print(name + " won 2 out of three rounds! Your mother should be proud!")
+        print(name + f" won {scores['User']} times out of three rounds! Your mother should be proud!")
         exit()
     if scores["CPU"] == scores["User"]:
         print(name + " and CPU tied! Way to bring the mediocrity. Please play again :)")
         exit()
-    print(name + " lost out of three rounds! Your father must be disappointed")
+    print(name + f" lost {scores['CPU']} times out of three rounds! Your father must be disappointed")
     exit()
 
 # collect input options
-print("Welcome to Gabe's Rock Paper Scissors Game!")
-name = None
-while not name:
-    name = input("Please enter your name: ")  
-print("Hello " + name)
+def main():
+    print("Welcome to Gabe's Rock Paper Scissors Game!")
+    global name
+    name = None
+    while not name:
+        name = input("Please enter your name: ").title()
+        if name == '':
+            name = getuser().title()
+    print("Hello " + name)
 
-Difficulty = None
+    Difficulty = None
 # validate input
-while Difficulty not in gameModes:
-    Difficulty = input(f"Please select difficulty: {gameModes} ")
+    print('''
+In random mode, the computer will choose a random selection each round. 
+
+In smart mode, the computer will base its choice on the statistics of the user's previous round.
+
+''')
+    while Difficulty not in gameModes:
+        x = 0
+        while x != len(gameModes):
+             print('\t', gameModes[x])
+             x += 1
+        Difficulty = input("Please select difficulty: ")
 
 # collect first round
-print("This game is best out of 3 rounds. Please make your first choice.")
-for roundNumber in range(1,4):
-    print(f'Round number: {roundNumber}, Your Score: {scores["User"]}, CPU Score: {scores["CPU"]}')
-    choice=None
-    while choice not in options:
-        choice = input(f"Options are {options} ")
-    userLastChoice = choice
-    if Difficulty == "smart" and roundNumber > 1:
-        CPUChoice = beats[userLastChoice]
-    else:
-        CPUChoice = random.choice(options)
-    print("CPU choose " + CPUChoice)
-    if choice == CPUChoice:
-        print("You tied")
-    elif beats[CPUChoice] == choice:
-        scores["CPU"] += 1
-        print("You lose")
-    else:
-        scores["User"] += 1
-        print("You win")
+    print("This game is best out of 3 rounds. Please make your first choice.")
+    for roundNumber in range(1,4):
+        print(f'Round number: {roundNumber}, Your Score: {scores["User"]}, CPU Score: {scores["CPU"]}')
+        choice=None
+        while choice not in options:
+            x = 0
+            while x != len(options):
+                print('\t', options[x])
+                x += 1
+            choice = input(f"> ")
+        
+        userLastChoice = choice
+        if Difficulty == "smart" and roundNumber > 1:
+            CPUChoice = beats[userLastChoice]
+        else:
+            CPUChoice = random.choice(options)
+        print("CPU choose " + CPUChoice)
+        if choice == CPUChoice:
+            print("You tied")
+        elif beats[CPUChoice] == choice:
+            scores["CPU"] += 1
+            print("You lose")
+        else:
+            scores["User"] += 1
+            print("You win")
 
+main()
 scoreProcess(scores)
-
